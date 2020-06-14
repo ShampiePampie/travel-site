c9f6beb5497ac3f024bc01e524b8202d72632049
Anke <anke@meiering.de>
2020-06-14 18:23:46 +0200

    Completed the header

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,5 +1,11 @@
 import '../styles/styles.css';
+import { isString } from 'lodash';
+import MobileMenu from './modules/MobileMenu.js';
+
+let mobileMenu = new MobileMenu();
+
 
 if(module.hot){
     module.hot.accept();
 }
+
