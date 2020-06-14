168b0eca6cb8e4130d55962051ee15047fb23ad7
Anke <anke@meiering.de>
2020-06-14 19:05:38 +0200

    reveal on scroll part 1

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,6 +1,9 @@
 import '../styles/styles.css';
 import { isString } from 'lodash';
 import MobileMenu from './modules/MobileMenu.js';
+import RevealOnScroll from './modules/RevealOnScroll.js';
+
+let revealOnScroll = new RevealOnScroll();
 
 let mobileMenu = new MobileMenu();
 
