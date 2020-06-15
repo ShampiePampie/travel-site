45d3099a4ea69f6cd78958a4aa282e3bba305440
Anke <anke@meiering.de>
2020-06-15 16:28:11 +0200

    finished picker... need to filter next

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,9 +1,22 @@
 import '../styles/styles.css';
 import { isString } from 'lodash';
-import MobileMenu from './modules/MobileMenu.js';
+/*import MobileMenu from './modules/MobileMenu.js';
 
-let mobileMenu = new MobileMenu();
+let mobileMenu = new MobileMenu();*/
 
+console.log("eventdate: " + document.querySelectorAll('.EventTile')[0].dataset.eventdate);
+
+var now = new Date();
+var dayNow = now.getDate();
+var monthNow = now.getMonth(); /* Januar = 0 */
+var yearNow = now.getFullYear();
+
+console.log(now);
+console.log(dayNow);
+console.log(monthNow);
+console.log(yearNow);
+
+/*new Date("{{dayFrom}} {{monthFromENLangString}} {{yearFrom}} {{timeFrom}}");*/
 
 if(module.hot){
     module.hot.accept();
