d9a76493a497f2aaf5fceb70211e9d65736fdf86
Anke <anke@meiering.de>
2020-06-15 19:44:49 +0200

    Completed the reveal on scroll behaviour

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -3,7 +3,8 @@ import { isString } from 'lodash';
 import MobileMenu from './modules/MobileMenu.js';
 import RevealOnScroll from './modules/RevealOnScroll.js';
 
-let revealOnScroll = new RevealOnScroll();
+new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
+new RevealOnScroll(document.querySelectorAll(".testimonial"), 60);
 
 let mobileMenu = new MobileMenu();
 
