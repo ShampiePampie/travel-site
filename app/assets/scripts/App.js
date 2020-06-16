da221975a9055cdb574f9df599807ea8576f43f7
Anke <anke@meiering.de>
2020-06-16 19:49:36 +0200

    completed the modal overlay

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -4,13 +4,30 @@ import MobileMenu from './modules/MobileMenu.js';
 import RevealOnScroll from './modules/RevealOnScroll.js';
 import StickyHeader from './modules/StickyHeader.js';
 
-let stickyHeader = new StickyHeader();
+
+
+new StickyHeader();
 
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 60);
 
-let mobileMenu = new MobileMenu();
+new MobileMenu();
+let modal;
 
+
+document.querySelectorAll('.open-modal').forEach(el => {
+    el.addEventListener("click", event => {
+        event.preventDefault();
+        if(typeof modal == "undefined"){
+            import(/* webpackChunkName: "modal" */'./modules/Modal').then(file => {
+                modal = new file.default();
+                setTimeout(() => modal.openTheModal(), 20);
+            }).catch(() => console.log("There was a problem."));
+        } else{
+            modal.openTheModal();
+        }
+    })
+})
 
 if(module.hot){
     module.hot.accept();
